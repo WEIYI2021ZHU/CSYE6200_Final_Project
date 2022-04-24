45d3bab7a509288b367a90738bfa2744ab69bef1
LucyZhao1999 <zhao.lu1@northeastern.edu>
2022-04-24 02:19:21 -0400

    m

--- 6200Final/src/UI/MainJPanel.java
+++ 6200Final/src/UI/MainJPanel.java
@@ -89,7 +89,10 @@ public class MainJPanel extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        StudentPortal cjp = new StudentPortal(mainJPanel);
+        mainJPanel.add(cjp);
+        CardLayout layout = (CardLayout)mainJPanel.getLayout();
+        layout.next(mainJPanel);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void VaccineAlertJButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_VaccineAlertJButtonActionPerformed

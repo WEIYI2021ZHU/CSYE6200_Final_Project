72e7fc00d093b279f036f3d8278576efb81604f3
WEIYI2021ZHU <zhu.weiy@northeastern.edu>
2022-04-29 20:52:44 -0400

    Final_version

--- CSYE6200FinalProject/src/edu/neu/csye6200/ClassroomFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.csye6200;
-
-public class ClassroomFactory {
-	public static Classroom getClassroom(int id, int minAge, int maxAge, int capacity) {
-		return new Classroom(id, minAge, maxAge, capacity);
-	}
-}
--- CSYE6200FinalProject/src/edu/neu/csye6200/VaccineFactory.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.csye6200;
-
-public class VaccineFactory {
-	public static Vaccine getVaccine(int id, String name, int minAge, int maxAge, int dose) {
-		return new Vaccine(id, name, minAge, maxAge, dose);
-	}
-}
